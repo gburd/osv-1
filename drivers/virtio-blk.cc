4422571b482ff2e08d1c5ab58534ec9609e61bc6
Greg Burd <greg@burd.me>
2026-03-20 13:29:31 -0400

    block: add TRIM/DISCARD and multiqueue I/O support

--- drivers/virtio-blk.cc
+++ drivers/virtio-blk.cc
@@ -117,7 +117,7 @@ bool blk::ack_irq()
 }
 
 blk::blk(virtio_device& virtio_dev)
-    : virtio_driver(virtio_dev), _ro(false)
+    : virtio_driver(virtio_dev), _ro(false), _num_queues(1)
 {
     _driver_name = "virtio-blk";
     _id = _instance++;
@@ -235,6 +235,18 @@ void blk::read_config()
         set_readonly();
         trace_virtio_blk_read_config_ro();
     }
+    if (get_guest_feature_bit(VIRTIO_BLK_F_MQ)) {
+        READ_CONFIGURATION_FIELD(blk_config,num_queues,_config.num_queues)
+        _num_queues = _config.num_queues;
+        virtio_i("virtio-blk: multiqueue with %d queues\n", _num_queues);
+    }
+    if (get_guest_feature_bit(VIRTIO_BLK_F_DISCARD)) {
+        READ_CONFIGURATION_FIELD(blk_config,max_discard_sectors,_config.max_discard_sectors)
+        READ_CONFIGURATION_FIELD(blk_config,max_discard_seg,_config.max_discard_seg)
+        READ_CONFIGURATION_FIELD(blk_config,discard_sector_alignment,_config.discard_sector_alignment)
+        virtio_i("virtio-blk: DISCARD support enabled (max_sectors=%u, max_seg=%u, alignment=%u)\n",
+                _config.max_discard_sectors, _config.max_discard_seg, _config.discard_sector_alignment);
+    }
 }
 
 void blk::req_done()
@@ -314,6 +326,13 @@ int blk::make_request(struct bio* bio)
         case BIO_FLUSH:
             type = VIRTIO_BLK_T_FLUSH;
             break;
+        case BIO_DISCARD:
+            if (!get_guest_feature_bit(VIRTIO_BLK_F_DISCARD)) {
+                biodone(bio, false);
+                return EOPNOTSUPP;
+            }
+            type = VIRTIO_BLK_T_DISCARD;
+            break;
         default:
             return ENOTBLK;
         }
@@ -327,7 +346,12 @@ int blk::make_request(struct bio* bio)
         queue->init_sg();
         queue->add_out_sg(hdr, sizeof(struct blk_outhdr));
 
-        if (bio->bio_data && bio->bio_bcount > 0) {
+        if (type == VIRTIO_BLK_T_DISCARD) {
+            req->discard_desc.sector = bio->bio_offset / sector_size;
+            req->discard_desc.num_sectors = bio->bio_bcount / sector_size;
+            req->discard_desc.flags = 0;
+            queue->add_out_sg(&req->discard_desc, sizeof(req->discard_desc));
+        } else if (bio->bio_data && bio->bio_bcount > 0) {
             if (type == VIRTIO_BLK_T_OUT)
                 queue->add_out_sg(bio->bio_data, bio->bio_bcount);
             else
@@ -354,7 +378,9 @@ u64 blk::get_driver_features()
                  | ( 1 << VIRTIO_BLK_F_RO)
                  | ( 1 << VIRTIO_BLK_F_BLK_SIZE)
                  | ( 1 << VIRTIO_BLK_F_CONFIG_WCE)
-                 | ( 1 << VIRTIO_BLK_F_WCE));
+                 | ( 1 << VIRTIO_BLK_F_WCE)
+                 | ( 1 << VIRTIO_BLK_F_MQ)
+                 | ( 1 << VIRTIO_BLK_F_DISCARD));
 }
 
 hw_driver* blk::probe(hw_device* dev)
